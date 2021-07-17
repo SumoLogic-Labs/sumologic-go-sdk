7b07a9e541c5ecbede90bf2487a8d83f2ec0c28e
Kyle Jackson <kyle@thepublicclouds.com>
2021-07-17 22:06:39 +1000

    troubleshooting event hub create command

--- service/cip/api_sources_CreateEventHubSource.go
+++ service/cip/api_sources_CreateEventHubSource.go
@@ -64,6 +64,7 @@ func (a *APIClient) CreateEventHubSource(body types.CreateEventHubSourceRequest,
 
 	localVarBody, err := io.ReadAll(localVarHttpResponse.Body)
 	localVarHttpResponse.Body.Close()
+	fmt.Println(string(localVarBody))
 	if err != nil {
 		return localVarReturnValue, localVarHttpResponse, err
 	}
