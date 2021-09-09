2a4fb6d74824bffd596d4a5be6dafb78b47e2694
Kyle Jackson <kyle@thepublicclouds.com>
2021-09-09 19:36:15 +1000

    troubleshooting send message

--- service/cip/api_client.go
+++ service/cip/api_client.go
@@ -302,6 +302,7 @@ func addFile(w *multipart.Writer, fieldName, path string) error {
 
 // Set request body from an interface{}
 func setBody(body interface{}, contentType string) (bodyBuf *bytes.Buffer, err error) {
+	fmt.Println(contentType)
 	if bodyBuf == nil {
 		bodyBuf = &bytes.Buffer{}
 	}
